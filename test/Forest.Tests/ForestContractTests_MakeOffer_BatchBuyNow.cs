3568274dee51e9eba251c02ac5ba7dec45a0b7c3
theodore <theodore@eforest.finance>
2023-12-14 10:00:51 +0800

    feat: fix ut

--- test/Forest.Tests/ForestContractTests_MakeOffer_BatchBuyNow.cs
+++ test/Forest.Tests/ForestContractTests_MakeOffer_BatchBuyNow.cs
@@ -227,6 +227,150 @@ public partial class ForestContractTests_MakeOffer
         #endregion
     }
     
+    
+    [Fact]
+    public async void BatchBuyNow_Buy_From_OneUser_Listing_Price_Not_Exist_Fail()
+    {
+        await InitializeForestContract();
+        await PrepareNftData();
+        #region basic begin
+        //seller user1 add listing
+        var user1ApproveQuantity = 0;
+        var user1InputListQuantity1 = 1;
+        var user1InputSellPrice1 = 2;
+        user1ApproveQuantity += user1InputListQuantity1;
+        var startTime1 = await InitUserListInfo(user1InputListQuantity1, user1InputSellPrice1, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        var user1InputListQuantity2 = 2;
+        var user1InputSellPrice2 = 3;
+        user1ApproveQuantity += user1InputListQuantity2;
+        var startTime2 = await InitUserListInfo(user1InputListQuantity2, user1InputSellPrice2, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity2, user1InputSellPrice2,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        //seller user3 add listing
+        var user3ApproveQuantity = 0;
+        var user3InputListQuantity1 = 1;
+        var user3InputSellPrice1 = 2;
+        user3ApproveQuantity += user3InputListQuantity1;
+        var startTime3 = await InitUserListInfo(user3InputListQuantity1, user3InputSellPrice1, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        var user3InputListQuantity2 = 2;
+        var user3InputSellPrice2 = 3;
+        user3ApproveQuantity += user3InputListQuantity2;
+        var startTime4 = await InitUserListInfo(user3InputListQuantity2, user3InputSellPrice2, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity2, user3InputSellPrice2,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        #endregion basic end
+
+        #region BatchBuyNow user2 buy from user1 listing records
+        
+        {
+            //modify BlockTime
+            var BlockTimeProvider = GetRequiredService<IBlockTimeProvider>();
+            BlockTimeProvider.SetBlockTime(BlockTimeProvider.GetBlockTime().AddMinutes(5));
+
+            var batchBuyNowInput = new BatchBuyNowInput();
+            batchBuyNowInput.Symbol = NftSymbol;
+            var fixPriceList = new RepeatedField<FixPriceList>(); 
+            var priceList1 = new FixPriceList()
+            {
+                StartTime = startTime1,
+                OfferTo = User1Address,
+                Quantity = user1InputListQuantity1,
+                Price = new Price()
+                {
+                    Amount = user1InputSellPrice1*3,
+                    Symbol = "ELF"
+                }
+            };
+            var priceList2 = new FixPriceList()
+            {
+                StartTime = startTime2,
+                OfferTo = User1Address,
+                Quantity = user1InputListQuantity2,
+                Price = new Price()
+                {
+                    Amount = user1InputSellPrice2*3,
+                    Symbol = "ELF"
+                }
+            };
+            fixPriceList.Add(priceList1);
+            fixPriceList.Add(priceList2);
+            batchBuyNowInput.FixPriceList.AddRange(fixPriceList);
+            
+            // user2 BatchBuyNow
+            var errorMessage = "";
+            try
+            {
+                await BuyerForestContractStub.BatchBuyNow.SendAsync(batchBuyNowInput);
+
+            }catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+            errorMessage.ShouldContain("NormalPrice does not exist.");
+        }
+
+        #endregion
+
+        // user1 nft number from 10 to 10
+        var user1NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User1Address
+        });
+        user1NftBalance.Output.Balance.ShouldBe(10);
+        // user3 nft number from 10 to 10
+        var user3NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User3Address
+        });
+        user3NftBalance.Output.Balance.ShouldBe(10);
+        
+        // user 2 nft number from 0 to 0
+        var user2NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User2Address
+        });
+        user2NftBalance.Output.Balance.ShouldBe(0);
+        
+        // 0 NFTs to offer list
+        #region check offer list
+
+        {
+            // list offers just sent
+            var offerList = BuyerForestContractStub.GetOfferList.SendAsync(new GetOfferListInput()
+            {
+                Symbol = NftSymbol,
+                Address = User2Address,
+            }).Result.Output;
+            offerList.Value.Count.ShouldBe(0);
+        }
+
+        #endregion
+    }
+    
+    
     [Fact]
     public async void BatchBuyNow_Buy_From_OneUser_Sender_Balance_Is_Zero_Success()
     {
@@ -1096,6 +1240,290 @@ public partial class ForestContractTests_MakeOffer
                 errorMessage = e.Message;
             }
             errorMessage.ShouldContain("Insufficient funds");
+        }
+
+        #endregion
+
+        // user1 nft number from 10 to 10
+        var user1NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User1Address
+        });
+        user1NftBalance.Output.Balance.ShouldBe(10);
+        // user3 nft number from 10 to 10
+        var user3NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User3Address
+        });
+        user3NftBalance.Output.Balance.ShouldBe(10);
+        
+        // user 2 nft number from 0 to 0
+        var user2NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User2Address
+        });
+        user2NftBalance.Output.Balance.ShouldBe(0);
+        
+        // 0 NFTs to offer list
+        #region check offer list
+
+        {
+            // list offers just sent
+            var offerList = BuyerForestContractStub.GetOfferList.SendAsync(new GetOfferListInput()
+            {
+                Symbol = NftSymbol,
+                Address = User2Address,
+            }).Result.Output;
+            offerList.Value.Count.ShouldBe(0);
+        }
+
+        #endregion
+    }
+    
+     [Fact]
+    public async void BatchBuyNow_Buy_From_TwoUser_Listing_Records_Symbol_Is_Null_Fail()
+    {
+        await InitializeForestContract();
+        await PrepareNftData();
+        #region basic begin
+        //seller user1 add listing
+        var user1ApproveQuantity = 0;
+        var user1InputListQuantity1 = 1;
+        var user1InputSellPrice1 = 100;
+        user1ApproveQuantity += user1InputListQuantity1;
+        var startTime1 = await InitUserListInfo(user1InputListQuantity1, user1InputSellPrice1, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        var user1InputListQuantity2 = 2;
+        var user1InputSellPrice2 = 200;
+        user1ApproveQuantity += user1InputListQuantity2;
+        var startTime2 = await InitUserListInfo(user1InputListQuantity2, user1InputSellPrice2, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity2, user1InputSellPrice2,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        //seller user3 add listing
+        var user3ApproveQuantity = 0;
+        var user3InputListQuantity1 = 3;
+        var user3InputSellPrice1 = 300;
+        user3ApproveQuantity += user3InputListQuantity1;
+        var startTime3 = await InitUserListInfo(user3InputListQuantity1, user3InputSellPrice1, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        var user3InputListQuantity2 = 2;
+        var user3InputSellPrice2 = 400;
+        user3ApproveQuantity += user3InputListQuantity2;
+        var startTime4 = await InitUserListInfo(user3InputListQuantity2, user3InputSellPrice2, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity2, user3InputSellPrice2,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        #endregion basic end
+
+        #region BatchBuyNow user2 buy from user1、user3 listing records
+        
+        {
+            //modify BlockTime
+            var BlockTimeProvider = GetRequiredService<IBlockTimeProvider>();
+            BlockTimeProvider.SetBlockTime(BlockTimeProvider.GetBlockTime().AddMinutes(5));
+
+            var batchBuyNowInput = new BatchBuyNowInput();
+            //batchBuyNowInput.Symbol = NftSymbol;
+            var fixPriceList = new RepeatedField<FixPriceList>(); 
+            var priceList1 = new FixPriceList()
+            {
+                StartTime = startTime1,
+                OfferTo = User1Address,
+                Quantity = user1InputListQuantity1,
+                Price = new Price()
+                {
+                    Amount = user1InputSellPrice1,
+                    Symbol = "ELF"
+                }
+            };
+            var priceList2 = new FixPriceList()
+            {
+                StartTime = startTime3,
+                OfferTo = User3Address,
+                Quantity = user3InputListQuantity1,
+                Price = new Price()
+                {
+                    Amount = user3InputSellPrice1,
+                    Symbol = "ELF"
+                }
+            };
+            fixPriceList.Add(priceList1);
+            fixPriceList.Add(priceList2);
+            batchBuyNowInput.FixPriceList.AddRange(fixPriceList);
+            
+            // user2 BatchBuyNow
+            var errorMessage = "";
+            try
+            {
+                await BuyerForestContractStub.BatchBuyNow.SendAsync(batchBuyNowInput);
+
+            }catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+            errorMessage.ShouldContain("Invalid symbol data");
+        }
+
+        #endregion
+
+        // user1 nft number from 10 to 10
+        var user1NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User1Address
+        });
+        user1NftBalance.Output.Balance.ShouldBe(10);
+        // user3 nft number from 10 to 10
+        var user3NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User3Address
+        });
+        user3NftBalance.Output.Balance.ShouldBe(10);
+        
+        // user 2 nft number from 0 to 0
+        var user2NftBalance = await UserTokenContractStub.GetBalance.SendAsync(new GetBalanceInput()
+        {
+            Symbol = NftSymbol,
+            Owner = User2Address
+        });
+        user2NftBalance.Output.Balance.ShouldBe(0);
+        
+        // 0 NFTs to offer list
+        #region check offer list
+
+        {
+            // list offers just sent
+            var offerList = BuyerForestContractStub.GetOfferList.SendAsync(new GetOfferListInput()
+            {
+                Symbol = NftSymbol,
+                Address = User2Address,
+            }).Result.Output;
+            offerList.Value.Count.ShouldBe(0);
+        }
+
+        #endregion
+    }
+    
+    [Fact]
+    public async void BatchBuyNow_Buy_From_TwoUser_Listing_Records_FixPriceList_Is_Null_Fail()
+    {
+        await InitializeForestContract();
+        await PrepareNftData();
+        #region basic begin
+        //seller user1 add listing
+        var user1ApproveQuantity = 0;
+        var user1InputListQuantity1 = 1;
+        var user1InputSellPrice1 = 100;
+        user1ApproveQuantity += user1InputListQuantity1;
+        var startTime1 = await InitUserListInfo(user1InputListQuantity1, user1InputSellPrice1, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        var user1InputListQuantity2 = 2;
+        var user1InputSellPrice2 = 200;
+        user1ApproveQuantity += user1InputListQuantity2;
+        var startTime2 = await InitUserListInfo(user1InputListQuantity2, user1InputSellPrice2, user1ApproveQuantity
+            , UserTokenContractStub, Seller1ForestContractStub, User1Address);
+        await QueryLastByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity2, user1InputSellPrice2,
+            User1Address);
+        await QueryFirstByStartAscListInfo(Seller1ForestContractStub, user1InputListQuantity1, user1InputSellPrice1,
+            User1Address);
+
+        //seller user3 add listing
+        var user3ApproveQuantity = 0;
+        var user3InputListQuantity1 = 3;
+        var user3InputSellPrice1 = 300;
+        user3ApproveQuantity += user3InputListQuantity1;
+        var startTime3 = await InitUserListInfo(user3InputListQuantity1, user3InputSellPrice1, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        var user3InputListQuantity2 = 2;
+        var user3InputSellPrice2 = 400;
+        user3ApproveQuantity += user3InputListQuantity2;
+        var startTime4 = await InitUserListInfo(user3InputListQuantity2, user3InputSellPrice2, user3ApproveQuantity
+            , User3TokenContractStub, Seller3ForestContractStub, User3Address);
+        await QueryLastByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity2, user3InputSellPrice2,
+            User3Address);
+        await QueryFirstByStartAscListInfo(Seller3ForestContractStub, user3InputListQuantity1, user3InputSellPrice1,
+            User3Address);
+
+        #endregion basic end
+
+        #region BatchBuyNow user2 buy from user1、user3 listing records
+        
+        {
+            //modify BlockTime
+            var BlockTimeProvider = GetRequiredService<IBlockTimeProvider>();
+            BlockTimeProvider.SetBlockTime(BlockTimeProvider.GetBlockTime().AddMinutes(5));
+
+            var batchBuyNowInput = new BatchBuyNowInput();
+            batchBuyNowInput.Symbol = NftSymbol;
+            var fixPriceList = new RepeatedField<FixPriceList>(); 
+            var priceList1 = new FixPriceList()
+            {
+                StartTime = startTime1,
+                OfferTo = User1Address,
+                Quantity = user1InputListQuantity1,
+                Price = new Price()
+                {
+                    Amount = user1InputSellPrice1,
+                    Symbol = "ELF"
+                }
+            };
+            var priceList2 = new FixPriceList()
+            {
+                StartTime = startTime3,
+                OfferTo = User3Address,
+                Quantity = user3InputListQuantity1,
+                Price = new Price()
+                {
+                    Amount = user3InputSellPrice1,
+                    Symbol = "ELF"
+                }
+            };
+            fixPriceList.Add(priceList1);
+            fixPriceList.Add(priceList2);
+            //batchBuyNowInput.FixPriceList.AddRange(fixPriceList);
+            
+            // user2 BatchBuyNow
+            var errorMessage = "";
+            try
+            {
+                await BuyerForestContractStub.BatchBuyNow.SendAsync(batchBuyNowInput);
+
+            }catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+            errorMessage.ShouldContain("Invalid input data");
         }
 
         #endregion
