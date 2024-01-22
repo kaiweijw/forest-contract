fe51f013c2e4256bd22e23894616fd957f4ca19b
Stephen01Zhang <ziang01.zhang@gmail.com>
2024-01-22 11:42:19 +0800

    feat: test blank

--- contract/Forest.Contracts.SymbolRegistrar/SymbolRegistrarContract_Action.cs
+++ contract/Forest.Contracts.SymbolRegistrar/SymbolRegistrarContract_Action.cs
@@ -212,6 +212,7 @@ namespace Forest.Contracts.SymbolRegistrar
             Assert(proxyAccount?.ProxyAccountHash != null, "ProxyAccountHash not existed.");
             State.ProxyAccountHash.Value = proxyAccount.ProxyAccountHash;
             return proxyAccount.ProxyAccountHash;
+
         }
     }
 }
