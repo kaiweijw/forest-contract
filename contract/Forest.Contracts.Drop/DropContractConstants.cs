9703202dcfc20220355e0639c11aa30551e1b892
kevin <kevin@eforest.finance>
2024-01-29 10:07:17 +0800

    view method

--- contract/Forest.Contracts.Drop/DropContractConstants.cs
+++ contract/Forest.Contracts.Drop/DropContractConstants.cs
@@ -2,11 +2,4 @@ namespace Forest.Contracts.Drop;
 
 public static class DropContractConstants
 {
-    public const string NFTSymbolSeparator = "-";
-    public const string CollectionSymbolSuffix = "0";
-
-    // 721 type NFT
-    public const long DefaultAmount = 1;
-
-    public const long TenThousandConstant = 10000;
 }
--- contract/Forest.Contracts.Drop/DropContract_View.cs
+++ contract/Forest.Contracts.Drop/DropContract_View.cs
@@ -16,4 +16,26 @@ public partial class DropContract
         return new Int32Value { Value = State.MaxDropDetailListCount.Value };
     }
     
+    public override DropInfo GetDropInfo(GetDropInfoInput input)
+    {
+        Assert(input != null && input.DropId != null && input.DropId != null && input.Index > 0, "Invalid input.");
+        var dropInfo =  State.DropInfoMap[input.DropId];
+        Assert(dropInfo != null, "Drop info not found.");
+        Assert(dropInfo.MaxIndex >= input.Index, "Invalid drop index.");
+
+        if(input.Index == 1)
+            return dropInfo;
+
+        var dropDetailList = State.DropDetailListMap[input.DropId][input.Index];
+        Assert(dropDetailList != null, "Drop detail list not found.");
+        dropInfo.DetailList = dropDetailList;
+        return dropInfo;
+    }
+    
+    public override ClaimDropDetail GetClaimDropInfo(GetClaimDropInfoInput input)
+    {
+        Assert(input != null && input.DropId != null && input.Address != null, "Invalid input.");
+        return State.ClaimDropMap[input.DropId][input.Address];
+    }
+
 }
